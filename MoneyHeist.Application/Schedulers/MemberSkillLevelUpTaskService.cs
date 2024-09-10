077bec1c8f47b9c59b78c872215095117794b5f2
DESKTOP-3BH8ML6\tomos <tomo.stjepovic@gmail.com>
2024-09-10 12:50:32 +0200

    Added member skill level up task service

---
+++ MoneyHeist.Application/Schedulers/MemberSkillLevelUpTaskService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MoneyHeist.Application.Interfaces;
+
+namespace MoneyHeist.Application.Schedulers
+{
+    public class MemberSkillLevelUpTaskService : BackgroundService
+    {
+        private readonly IServiceScopeFactory serviceScopeFactory;
+        private Timer _timer;
+
+        public MemberSkillLevelUpTaskService(IServiceScopeFactory _serviceScopeFactory)
+        {
+            serviceScopeFactory = _serviceScopeFactory;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            return Task.CompletedTask;
+        }
+
+        private async void DoWork(object state)
+        {
+            using IServiceScope scope = serviceScopeFactory.CreateScope();
+
+            // not using DI becouse this service is added as a singleton, and heist service and dbContext are scoped
+            var heistService = scope
+                .ServiceProvider
+                .GetRequiredService<IHeistService>();
+            await heistService.HeistMembersLevelUp();
+        }
+
+        public override Task StopAsync(CancellationToken stoppingToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return base.StopAsync(stoppingToken);
+        }
+
+        public override void Dispose()
+        {
+            _timer?.Dispose();
+            base.Dispose();
+        }
+    }
+}
--- MoneyHeist/Program.cs
+++ MoneyHeist/Program.cs
@@ -21,6 +21,7 @@ namespace MoneyHeist
 
             // Register the hosted service
             builder.Services.AddHostedService<HeistStartAndFinishTaskService>();
+            builder.Services.AddHostedService<MemberSkillLevelUpTaskService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
